341d03c171e0d33c219bc74bf958a866c3e04e94
David J. Heberle <davidjheberle@gmail.com>
2017-01-15 10:28:29 -0500

    Initial commit.

---
+++ js/screens/play.js
@@ -0,0 +1,25 @@
+game.PlayScreen = me.ScreenObject.extend({
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        // reset the score
+        // game.data.score = 0;
+
+        // Add our HUD to the game world, add it last so that this is on top of the rest.
+        // Can also be forced by specifying a "Infinity" z value to the addChild function.
+        // this.HUD = new game.HUD.Container();
+        // me.game.world.addChild(this.HUD);
+
+        me.game.world.addChild(me.pool.pull("player"));
+        console.log("play");
+    },
+
+    /**
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        // remove the HUD from the game world
+        // me.game.world.removeChild(this.HUD);
+    }
+});
