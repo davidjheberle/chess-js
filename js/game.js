76392d517ea4a863d398667f6c6b3e59bc9d79e7
David J. Heberle <davidjheberle@gmail.com>
2017-02-07 20:09:44 -0500

    Added player and enemies to space invader tutorial.

--- js/game.js
+++ js/game.js
@@ -1,26 +1,25 @@
 
 /* Game namespace */
 var game = {
-
-    // an object where to store game information
-    data : {
-        // score
-        score : 0
-    },
-
-
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
+        }
+
+        // Add "#debug" to the URL to enable the debug Panel.
+        if (me.game.HASH.debug === true) {
+            window.onReady(function () {
+                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+            });
         }
 
         // Initialize the audio.
         me.audio.init("mp3,ogg");
 
-        // set and load all resources.
+        // Set and load all resources.
         // (this will also automatically switch to the loading screen)
         me.loader.preload(game.resources, this.loaded.bind(this));
     },
@@ -28,12 +27,12 @@ var game = {
     // Run on game resources loaded.
     "loaded" : function () {
 
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
-
-        // add our player entity in the entity pool
-        // me.pool.register("mainPlayer", game.PlayerEntity);
+        // Add entities to the entity pool.
         me.pool.register("player", game.Player);
+        me.pool.register("enemy", game.Enemy);
+
+        // Create the play screen.
+        me.state.set(me.state.PLAY, new game.PlayScreen());
 
         // Start the game.
         me.state.change(me.state.PLAY);
