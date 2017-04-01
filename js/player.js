e520ee07f3bbf0e1b9691ab488a0793b7f858a3d
David J. Heberle <davidjheberle@gmail.com>
2017-04-01 18:12:49 -0400

    added player input.

--- js/player.js
+++ js/player.js
@@ -6,5 +6,22 @@ game.Player = me.Sprite.extend({
             me.game.viewport.height - image.height - 20,
             { image : image }
         ]);
+        this.velx = 450;
+        this.maxX = me.game.viewport.width - this.width;
+    },
+
+    update : function (time) {
+        this._super(me.Sprite, "update", [time]);
+        if (me.input.isKeyPressed("left")) {
+            this.pos.x -= this.velx * time / 1000;
+        }
+
+        if (me.input.isKeyPressed("right")) {
+            this.pos.x += this.velx * time / 1000;
+        }
+
+        this.pos.x = this.pos.x.clamp(0, this.maxX);
+
+        return true;
     }
 })
--- js/screens/play.js
+++ js/screens/play.js
@@ -11,11 +11,22 @@ game.PlayScreen = me.ScreenObject.extend({
 
         // An an enemy.
         me.game.world.addChild(me.pool.pull("enemy", 50, 50), 2);
+
+        // Bind input.
+        me.input.bindKey(me.input.KEY.LEFT, "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.A, "left");
+        me.input.bindKey(me.input.KEY.D, "right");
     },
 
     /**
      * action to perform when leaving this screen (state change)
      */
     onDestroyEvent : function () {
+        // Unbind input.
+        me.input.unbindKey(me.input.KEY.LEFT);
+        me.input.unbindKey(me.input.KEY.RIGHT);
+        me.input.unbindKey(me.input.KEY.A);
+        me.input.unbindKey(me.input.KEY.D);
     }
 });
